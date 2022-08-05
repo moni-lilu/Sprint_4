805c8051ba5bf75655bb54a7ccd19f8d03230a8a
moni-lilu <moni-lilu@yandex.ru>
2022-08-05 11:03:32 +0100

    cleaned up code

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -8,37 +8,6 @@ public class Account {
         this.name = name;
     }
 
- //   public boolean checkNameToEmboss() {
-        /*
-             Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
-             Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
-         */
-/*        if (name.equals("")) {
-            return false;
-        } else {
-            String firstSymbol = String.valueOf(name.charAt(0));
-            String lastSymbol = String.valueOf(name.charAt(name.length() - 1));
-            int numberOfSpaces = 0;
-
-            for (int i = 0; i < name.length(); i++) {
-                if (String.valueOf(name.charAt(i)).equals(" ")) {
-                    numberOfSpaces = numberOfSpaces + 1;
-                }
-            }
-
-            if (name.length() < 3
-                    || name.length() > 19
-                    || numberOfSpaces == 0
-                    || numberOfSpaces > 1
-                    || firstSymbol.equals(" ")
-                    || lastSymbol.equals(" ")) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-    }
-*/
     public boolean checkNameToEmboss() {
         boolean isValidStringLength;
         boolean isValidSpaceLocation;
